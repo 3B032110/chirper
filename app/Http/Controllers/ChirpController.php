769416c582ef8e007b8254b063ec4d31fe745694
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-11-28 21:09:04 +0800

    Change chirps push to chrips.index view

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -18,7 +18,10 @@ class ChirpController extends Controller
     {
         //
         // return response('Hello, World!');
-        return view('chirps.index');
+        // return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
     }
 
     /**
